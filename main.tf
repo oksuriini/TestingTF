9922396e53e7fbbac82e8c920c6a9f8892b5667c
oksuriini <oskari.peiponen@hotmail.com>
2024-04-02 12:31:04 +0300

    added deployment

--- main.tf
+++ main.tf
@@ -6,6 +6,36 @@ terraform {
   }
 }
 
+resource "kubernetes_deployment" "nginx" {
+  metadata {
+    name      = "nginx-deployment"
+    namespace = "flux-system"
+  }
+  spec {
+    selector {
+      match_labels = {
+        app = "nginx"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app = "nginx"
+        }
+      }
+      spec {
+        container {
+          name  = "nginx-cont"
+          image = "nginx"
+          port {
+            container_port = 80
+          }
+        }
+      }
+    }
+  }
+}
+
 resource "kubernetes_pod" "example" {
   metadata {
     name      = "example"
